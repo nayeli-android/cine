1e6287a062bfbd2c52789ba0afe2c37970ff2217
nayeli-android <nayeliortiz013@gmail.com>
2024-08-20 00:20:55 -0600

    Add files via upload

---
+++ pagina1/index.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'; ?>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<h2>Bienvenido a nuestro Marketplace</h2>
+<p>Explora nuestros productos y encuentra las mejores ofertas.</p>
+<?php include 'footer.php'; ?>
